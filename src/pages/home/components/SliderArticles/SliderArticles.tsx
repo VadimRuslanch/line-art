08a96a512896eb6be2eccfe534d03eed14cba37a
VadimLebedev <Merzlyy.Pes@yandex.ru>
2025-03-12 18:54:48 +0300

    Update Sliders, buttons

--- src/pages/home/components/SliderArticles/SliderArticles.tsx
+++ src/pages/home/components/SliderArticles/SliderArticles.tsx
@@ -3,8 +3,32 @@ import styles from './SliderArticles.module.scss';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation } from 'swiper/modules';
+import SwiperNavButtonPrev from '@/shared/ui/Swiper/SwiperNavButtonPrev';
+import SwiperNavButtonNext from '@/shared/ui/Swiper/SwiperNavButtonNext';
+import { useEffect, useRef, useState } from 'react';
 
 export default function SliderArticles() {
+  type ButtonRef = HTMLButtonElement | null;
+
+  interface NavigationConfig {
+    prevEl: ButtonRef;
+    nextEl: ButtonRef;
+  }
+
+  const prevRef = useRef<ButtonRef>(null);
+  const nextRef = useRef<ButtonRef>(null);
+  const [navigationConfig, setNavigationConfig] = useState<NavigationConfig>({
+    prevEl: null,
+    nextEl: null,
+  });
+
+  useEffect(() => {
+    // Когда кнопки смонтированы, обновляем конфигурацию навигации
+    setNavigationConfig({
+      prevEl: prevRef.current,
+      nextEl: nextRef.current,
+    });
+  }, []);
   return (
     <section className={`${styles.container}`}>
       <header>
@@ -12,6 +36,7 @@ export default function SliderArticles() {
       </header>
       <Swiper
         modules={[Navigation]}
+        navigation={navigationConfig}
         className={styles.slider}
         spaceBetween="20"
         breakpoints={{
@@ -37,8 +62,10 @@ export default function SliderArticles() {
             <img className={styles.image} src="/images/product.png" alt="art" />
 
             <div>
-              <span className={styles.text}>Теневой профиль</span>
+              <span className={styles.text}>Статьи / . 14 Июн 2024</span>
             </div>
+
+            <p className={styles.name}>Алюминий в современном интерьере</p>
           </article>
         </SwiperSlide>
         <SwiperSlide className={styles.slide}>
@@ -46,8 +73,10 @@ export default function SliderArticles() {
             <img className={styles.image} src="/images/product.png" alt="art" />
 
             <div>
-              <span className={styles.text}>Теневой профиль</span>
+              <span className={styles.text}>Статьи / . 14 Июн 2024</span>
             </div>
+
+            <p className={styles.name}>Алюминий в современном интерьере</p>
           </article>
         </SwiperSlide>
         <SwiperSlide className={styles.slide}>
@@ -55,10 +84,28 @@ export default function SliderArticles() {
             <img className={styles.image} src="/images/product.png" alt="art" />
 
             <div>
-              <span className={styles.text}>Теневой профиль</span>
+              <span className={styles.text}>Статьи / . 14 Июн 2024</span>
             </div>
+
+            <p className={styles.name}>Алюминий в современном интерьере</p>
           </article>
         </SwiperSlide>
+        <SwiperSlide className={styles.slide}>
+          <article className={styles.content}>
+            <img className={styles.image} src="/images/product.png" alt="art" />
+
+            <div>
+              <span className={styles.text}>Статьи / . 14 Июн 2024</span>
+            </div>
+
+            <p className={styles.name}>Алюминий в современном интерьере</p>
+          </article>
+        </SwiperSlide>
+
+        <div className={styles.sliderControl}>
+          <SwiperNavButtonPrev ref={prevRef} />
+          <SwiperNavButtonNext ref={nextRef} />
+        </div>
       </Swiper>
     </section>
   );
