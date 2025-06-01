cee47fff5dc4a3254dc8116fae700526d602df24
VadimLebedev <Merzlyy.Pes@yandex.ru>
2025-06-01 13:58:12 +0300

    fix ButtonToBack

--- src/shared/ui/ButtonToBack/ButtonToBack.tsx
+++ src/shared/ui/ButtonToBack/ButtonToBack.tsx
@@ -1,9 +1,12 @@
 import styles from './ButtonToBack.module.scss';
 import IconArrow from '@/shared/assets/svg/arrow-left.svg';
+import { useRouter } from 'next/navigation';
 
 export default function ButtonToBack() {
+  const router = useRouter();
+
   return (
-    <button className={styles.button}>
+    <button onClick={router.back} className={styles.button}>
       <IconArrow />
       Назад
     </button>
